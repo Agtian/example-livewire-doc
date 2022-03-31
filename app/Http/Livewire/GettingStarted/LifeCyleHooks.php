bf1fefc6f58f90cc21864d95ede542ecf0efa01d
agtian <agtianrickey33@gmail.com>
2022-03-31 11:14:43 +0700

    add folder getting-started and lifecylehook

---
+++ app/Http/Livewire/GettingStarted/LifeCyleHooks.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire\GettingStarted;
+
+use Livewire\Component;
+
+class LifeCyleHooks extends Component
+{
+    public function render()
+    {
+        return view('livewire.getting-started.life-cyle-hooks');
+    }
+}
