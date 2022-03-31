1fea21638c7159fcfcd8214355daea5967668a57
agtian <agtianrickey33@gmail.com>
2022-03-31 11:00:04 +0700

    Building off the data-binding concept, action in Livewire

--- resources/views/livewire/counter.blade.php
+++ resources/views/livewire/counter.blade.php
@@ -1,6 +1,6 @@
 <div class="container">
     <div class="row mt-3">
-        <div class="col-lg-4">
+        <div class="col-lg-3">
             <input type="text" class="form-control" wire:model="name">
         </div>
         <div class="col-lg-1">
@@ -21,6 +21,11 @@
                 <option>Football</option>
                 <option>Swimming</option>
             </select>
+        </div>
+        <div class="col-lg-2">
+            <form action="#" wire:submit.prevent="$set('name', '')">
+                <button class="btn btn-md btn-danger">Reset Name</button>
+            </form>
         </div>
     </div>
 
